dbc32520df081768026b217459cd3b5886fbc6e0
drissakande <drissakande@gmail.com>
2023-05-09 08:33:45 -0400

    Add code in index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+
+  fetch(`http://localhost:3000/movies/${input.value}`)
+  .then(function(res){
+    return res.json();
+
+  })
+  .then(function(data){
+    const title = document.querySelector("section#movieDetails h4");
+     const summary = document.querySelector("section#movieDetails p");
+     title.innerText = data.title;
+    summary.innerText = data.summary;
+  })
+  })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
